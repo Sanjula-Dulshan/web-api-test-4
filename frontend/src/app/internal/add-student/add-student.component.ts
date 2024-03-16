33a32c90e3bbe5460c6f1b2f3bda14f555dde859
Sanjula-Dulshan <sdulshan10@gmail.com>
2024-03-16 09:51:48 +0530

    get stream data to dropdown

--- frontend/src/app/internal/add-student/add-student.component.ts
+++ frontend/src/app/internal/add-student/add-student.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { StreamsService } from '../streams.service';
 
 @Component({
   selector: 'app-add-student',
   templateUrl: './add-student.component.html',
-  styleUrls: ['./add-student.component.scss']
+  styleUrls: ['./add-student.component.scss'],
 })
 export class AddStudentComponent {
+  streams: any[] = [];
 
+  constructor(private streamService: StreamsService) {}
+
+  ngOnInit(): void {
+    this.streamService.getStreams().subscribe({
+      next: (res: any) => {
+        this.streams = res;
+        console.log('res>> ', this.streams);
+      },
+      error: (err) => {
+        console.log('error>>> ', err);
+      },
+    });
+  }
 }
