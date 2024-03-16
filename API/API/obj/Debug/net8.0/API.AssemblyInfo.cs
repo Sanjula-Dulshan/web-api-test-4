b7aae2fe5e1639b99e1a624876a35dc38687be34
Sanjula-Dulshan <sdulshan10@gmail.com>
2024-03-16 09:23:35 +0530

    init add student component

--- API/API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53999834394338744164fbc8966dc02f1a8e17c2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bfce27251f2789132f754ea9c3368180a4ed4bf8")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- frontend/src/app/internal/internal.module.ts
+++ frontend/src/app/internal/internal.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { InternalRoutingModule } from './internal-routing.module';
 import { StreamsComponent } from './streams/streams.component';
+import { AddStudentComponent } from './add-student/add-student.component';
 
 @NgModule({
-  declarations: [StreamsComponent],
+  declarations: [StreamsComponent, AddStudentComponent],
   imports: [CommonModule, InternalRoutingModule],
 })
 export class InternalModule {}
