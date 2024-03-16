eb3edbc9ef62e1d9d8451840a314f3846fb097d4
Sanjula-Dulshan <sdulshan10@gmail.com>
2024-03-16 11:54:50 +0530

    complete add student ui

--- frontend/src/app/internal/add-student/add-student.component.ts
+++ frontend/src/app/internal/add-student/add-student.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { StreamsService } from '../streams.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Student } from 'interface';
 
 @Component({
   selector: 'app-add-student',
@@ -7,19 +9,37 @@ import { StreamsService } from '../streams.service';
   styleUrls: ['./add-student.component.scss'],
 })
 export class AddStudentComponent {
+  studentForm!: FormGroup;
   streams: any[] = [];
 
-  constructor(private streamService: StreamsService) {}
+  constructor(private streamService: StreamsService, private fb: FormBuilder) {}
 
   ngOnInit(): void {
+    this.studentForm = this.fb.group({
+      firstName: ['', Validators.required],
+      secondName: [''],
+      lastName: ['', Validators.required],
+      dateOfBirth: ['', Validators.required],
+      stream: ['', Validators.required],
+      gender: ['', Validators.required],
+    });
+
     this.streamService.getStreams().subscribe({
       next: (res: any) => {
         this.streams = res;
-        console.log('res>> ', this.streams);
       },
       error: (err) => {
         console.log('error>>> ', err);
       },
     });
+  }
+
+  onSubmit(): void {
+    if (this.studentForm.valid) {
+      const newStudent: Student = this.studentForm.value;
+      console.log('newStudent>> ', newStudent);
+    } else {
+      console.log('form is invalid');
+    }
   }
 }
--- frontend/src/app/internal/internal.module.ts
+++ frontend/src/app/internal/internal.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { InternalRoutingModule } from './internal-routing.module';
 import { StreamsComponent } from './streams/streams.component';
 import { AddStudentComponent } from './add-student/add-student.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [StreamsComponent, AddStudentComponent],
-  imports: [CommonModule, InternalRoutingModule],
+  imports: [CommonModule, InternalRoutingModule, ReactiveFormsModule],
 })
 export class InternalModule {}
